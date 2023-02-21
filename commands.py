98d0fdd225fdc141d1f0ca97f7e7a2cda023258a
Andrew Kozempel <104046017+itzamk@users.noreply.github.com>
2023-02-20 20:18:36 -0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,42 +1,25 @@
-import subprocess
+import os
 import time
 
-# Start roscore
-subprocess.Popen(['roscore'])
+os.system('gnome-terminal -- roscore')
+time.sleep(2)
 
-# Wait for a few seconds to let roscore start
+os.system('gnome-terminal -- bash -c "export TURTLEBOT3_MODEL=waffle_pi; roslaunch turtlebot3_gazebo turtlebot3_world.launch"')
+time.sleep(30)
+
+os.system('gnome-terminal -- bash -c "export TURTLEBOT3_MODEL=waffle_pi; roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping"')
+time.sleep(30)
+
+os.system('gnome-terminal -- roslaunch turtlebot3_gazebo turtlebot3_simulation.launch')
+time.sleep(600)
+
+os.system('gnome-terminal -- rosrun map_server map_saver -f ~/map_test')
 time.sleep(5)
 
-# Launch Gazebo world
-subprocess.Popen(['export', 'TURTLEBOT3_MODEL=waffle_pi'])
-subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 'turtlebot3_world.launch'])
-time.sleep(30)
-
-# Launch SLAM
-subprocess.Popen(['export', 'TURTLEBOT3_MODEL=waffle_pi'])
-subprocess.Popen(['roslaunch', 'turtlebot3_slam', 'turtlebot3_slam.launch', 'slam_methods:=gmapping'])
-time.sleep(30)
-
-# Launch Gazebo simulation
-subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 'turtlebot3_simulation.launch'])
-
-# Wait for 5 minutes
-time.sleep(300)
-
-# Save map
-subprocess.Popen(['rosrun', 'map_server', 'map_saver', '-f', '~/map_test'])
+os.system('pkill -f "roslaunch"')
 time.sleep(5)
 
-# Kill all processes
-subprocess.Popen(['killall', 'roslaunch'])
-subprocess.Popen(['killall', 'gzserver'])
-subprocess.Popen(['killall', 'gzclient'])
-
-# Launch Gazebo world with saved map
-subprocess.Popen(['export', 'TURTLEBOT3_MODEL=waffle_pi'])
-subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 'turtlebot3_world.launch'])
+os.system('gnome-terminal -- bash -c "export TURTLEBOT3_MODEL=waffle_pi; roslaunch turtlebot3_gazebo turtlebot3_world.launch"')
 time.sleep(30)
 
-# Launch navigation with saved map
-subprocess.Popen(['export', 'TURTLEBOT3_MODEL=waffle_pi'])
-subprocess.Popen(['roslaunch', 'turtlebot3_navigation', 'turtlebot3_navigation.launch', 'map_file:=$HOME/map_test.yaml'])
+os.system('gnome-terminal -- bash -c "export TURTLEBOT3_MODEL=waffle_pi; roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map_test.yaml"')
